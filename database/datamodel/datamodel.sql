ea42e4c3052f15c4444a5b7b18cc82695361ce6c
programmatoroSeduto <francesc.ganci@gmail.com>
2023-08-18 20:05:14 +0200

    design data model

--- database/datamodel/datamodel.sql
+++ database/datamodel/datamodel.sql
@@ -29,6 +29,14 @@ About suffixes: it indicates the type of the column
 - _FK : foreign key
     - when present, the column naming convention is <table name>_FK
 
+## Tables Naming Conventions
+
+tables are identified by the prefix of the name:
+
+- D_ : dimensions (describing entities, ES: list of users and their data)
+- F_ : facts (records generated in time, ES: a log table)
+- L_ : lookup (relations between objects, ES: user holding a device)
+
 ## ID and codes
 
 There are some conventions to recall, depending on what
@@ -195,6 +203,7 @@ provided request is suspicious or potentially dangerous.
 
 DROP TABLE IF EXISTS sar.D_USER;
 CREATE TABLE sar.D_USER (
+
     USER_ID CHAR(24) NOT NULL
     , USER_IS_EXTERNAL_FL BOOLEAN NOT NULL
         DEFAULT false
@@ -205,6 +214,7 @@ CREATE TABLE sar.D_USER (
         DEFAULT NULL
     , USER_ADDRESS_DS VARCHAR(100)
         DEFAULT NULL
+    , USER_HEIGHT_VL FLOAT(2) NOT NULL
 
     -- admins have different checks 
     , USER_ID_ADMIN_FL BOOLEAN NOT NULL
@@ -237,6 +247,7 @@ CREATE TABLE sar.D_USER (
         DEFAULT NULL
     
     , PRIMARY KEY ( USER_ID )
+
 );
 
 DROP TABLE IF EXISTS sar.D_USER_ACCESS_CODE;
@@ -252,6 +263,7 @@ CREATE TABLE sar.D_USER_ACCESS_CODE (
         DEFAULT false
 
     , PRIMARY KEY ( USER_ID )
+
 )
 
 DROP TABLE IF EXISTS sar.F_USER_ACTIVITY;
@@ -269,6 +281,7 @@ CREATE TABLE sar.F_USER_ACTIVITY (
     , USER_ACCESS_TOKEN_ID TEXT NOT NULL
 
     , PRIMARY KEY ( USER_ID, START_AT_TS )
+
 );
 
 
@@ -300,8 +313,8 @@ CREATE SEQUENCE sar.F_ACTIVITY_LOG_SEQUENCE
 DROP TABLE IF EXISTS sar.F_ACTIVITY_LOG;
 CREATE TABLE sar.F_ACTIVITY_LOG (
 
-    F_ACTIVITY_LOG_PK CHAR(8) NOT NULL
-        DEFAULT LPAD(CAST(nextval('sar.F_ACTIVITY_LOG_SEQUENCE') AS TEXT), 8, '0')
+    F_ACTIVITY_LOG_PK CHAR(32) NOT NULL
+        DEFAULT LPAD(CAST(nextval('sar.F_ACTIVITY_LOG_SEQUENCE') AS TEXT), 32, '0')
     
     , LOG_CREATION_TS TIMESTAMP NOT NULL
         DEFAULT CURRENT_TIMESTAMP
@@ -325,6 +338,7 @@ CREATE TABLE sar.F_ACTIVITY_LOG (
         DEFAULT false
 
     , PRIMARY KEY ( F_ACTIVITY_LOG_PK )
+
 );
 
 
@@ -399,7 +413,11 @@ here are the capabilities a device can have in this data model:
     - USAGE_AUTONOMOUS : it is a autonomous device
     - USAGE_WEARABLE : wearable device
 
-## TRANSACTION (get-device) : DEVICE assign to user
+### Hold a device
+
+it is sufficient to have a active user session to hold a device with a given ID. 
+
+## TRANSACTION (access) : DEVICE assign to user
 
 API: api/device/login
     - REQUEST:
@@ -413,13 +431,27 @@ The request is performed directly by the user, once enabled a
 session token. Here are the steps:
 
 - (CHECK) user
-    - if the user exists
-    - if the user is active
-    - if the session ID is still valid
+    - (D_USER) if the user exists
+    - (F_USER_ACTIVITY) if the user is active
+    - (F_USER_ACTIVITY) if the session ID is still valid
 - (CHECK) device
-    - if the device is holdable -> DEVICE_IS_HOLDABLE_FL
-    - if the user is allowed to hold the device
+    - (D_DEVICE) if the device exists
+    - (D_DEVICE) if the device is holdable -> DEVICE_IS_HOLDABLE_FL
+    - (L_DEVICE_USER) if the user is allowed to hold the device
+    - (F_DEVICE_ACTIVITY) if the device is not hold by another user
 - (UPDATE) device session data
+    - (F_DEVICE_ACTIVITY) open a new row with the session ID
+    - (F_ACTIVITY_LOG) register the activity
+
+## TRANSACTION (release) : DEVICE release
+
+API: api/device/logout
+    - REQUEST:
+        - user id
+        - device id
+        - user session token
+    - RESPONSE:
+        - OK or not OK ...?
 
 ====================================================== */
 
@@ -496,3 +528,311 @@ CREATE TABLE sar.L_DEVICE_USER (
 
     , PRIMARY KEY ( L_DEVICE_USER_PK )
 );
+
+DROP TABLE IF EXISTS sar.F_DEVICE_ACTIVITY;
+CREATE TABLE sar.F_DEVICE_ACTIVITY (
+
+    DEVICE_ID CHAR(24) NOT NULL
+    , USER_ACCESS_TOKEN_ID TEXT NOT NULL
+    
+    -- operation timestamps
+    , DEVICE_ON_AT_TS TIMESTAMP NOT NULL
+        DEFAULT CURRENT_TIMESTAMP
+    , DEVICE_OFF_AT_TS TIMESTAMP 
+
+    , PRIMARY KEY ( DEVICE_ID, USER_ACCESS_TOKEN_ID )
+
+)
+
+
+
+
+
+
+
+
+
+
+/* ======================================================
+
+## Hololens2 Support
+
+Here are the requirements for the datamodel for HoloLens2 in this implementation:
+
+- device settings per user -> D_HL2_USER_DEVICE_SETTINGS
+    - the device can import/export its settings, for example the height of the user
+- positions export from the position database -> F_HL2_STAGING_WAYPOINTS, F_HL2_QUALITY_WAYPOINTS
+    - related to one user and one device
+    - related to one known reference position
+- links from the position database -> F_HL2_STAGING_PATHS, F_HL2_QUALITY_PATHS
+    - related to one user nd one device
+- reference positions -> D_HL2_REFERENCE_POSITIONS
+    - at least ID and operative protocol to determine the calibration
+    - in addition, the geolocation if available
+- transformations between reference positions -> L_HL2_REFERENCE_TRANSFORMATIONS
+- service status -> F_HL2_SERVICE_STATUS
+    - used for detecting for instance when a device is not reporting for 
+        too much time (it could mean something strange with the operator)
+
+Here are the transactions to implement:
+
+- get/set device settings
+- get reference position for the device (it has been already set by the control unit)
+- send calibration done -> obtain near positions (first calibration)
+- get near positions 
+- service status and last operation status available 
+
+### positions collection and data integration 
+
+(proposal, not implemented)
+
+the positions connection is divided into two tables:
+
+- STAGING : the table collects the direct export from the device
+- QUALITY : the positions are collected, cleanned and made available to the
+    other devices. A data integrator moves data from one table to another one
+
+In this implementation, the data integrator is very simple, but in a real 
+situation it will deal with a number of problems on the data:
+
+- data skew : due to a not perfect first calibration, a certain amout of error
+    affects the measurements
+- geolocation association : for the devices not able to obtain directly latitude
+    and longitude, these informations could be integrated by this module
+- ... 
+
+====================================================== */
+
+DROP TABLE IF EXISTS sar.D_HL2_USER_DEVICE_SETTINGS;
+CREATE TABLE sar.D_HL2_USER_DEVICE_SETTINGS (
+
+    DEVICE_ID CHAR(24) NOT NULL
+    , USER_ID CHAR(24) NOT NULL
+
+    -- settings
+
+    , PRIMARY KEY ( DEVICE_ID, USER_ID )
+);
+
+DROP TABLE IF EXISTS sar.F_HL2_STAGING_WAYPOINTS;
+CREATE TABLE sar.F_HL2_STAGING_WAYPOINTS (
+
+    DEVICE_ID CHAR(24) NOT NULL
+    , USER_ACCESS_TOKEN_ID TEXT NOT NULL
+
+    -- area center
+    , REFERENCE_POSITION_ID TEXT NOT NULL
+    , U_LEFT_HANDED_REFERENCE_FL BOOLEAN NOT NULL
+        DEFAULT true
+    , U_X FLOAT(15) NOT NULL
+        DEFAULT 0.00
+    , U_Y FLOAT(15) NOT NULL
+        DEFAULT 0.00
+    , U_Z FLOAT(15) NOT NULL
+        DEFAULT 0.00
+    , U_SOURCE_FROM_SERVER_FL BOOLEAN NOT NULL
+        DEFAULT false
+    
+    -- measures
+    , LOCAL_POSITION_ID INT NOT NULL
+    , LOCAL_AREA_INDEX_ID INT NOT NULL
+    , AREA_RADIUS_VL FLOAT(4) NOT NULL
+        DEFAUL 0.5
+    
+    -- metadata
+    , CREATED_TS TIMESTAMP NOT NULL
+        DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP SEQUENCE IF EXISTS sar.F_HL2_QUALITY_WAYPOINTS_SEQUENCE;
+CREATE SEQUENCE sar.F_HL2_QUALITY_WAYPOINTS_SEQUENCE
+    AS BIGINT
+    INCREMENT BY 1
+    START WITH 1
+;
+
+DROP TABLE IF EXISTS sar.F_HL2_QUALITY_WAYPOINTS;
+/*
+    U_ : relative position (wrt REFERENCE_POSITION_ID)
+    G_ : geo positions
+*/
+CREATE TABLE sar.F_HL2_QUALITY_WAYPOINTS (
+
+    F_HL2_QUALITY_WAYPOINTS_PK INT NOT NULL
+        DEFAULT nextval('F_HL2_QUALITY_WAYPOINTS_SEQUENCE')
+
+    -- SOURCE : the first user/device which found the position
+    , SOURCE_USER_ID CHAR(24) NOT NULL
+    , SOURCE_DEVICE_ID CHAR(24) NOT NULL
+    , SOURCE_REFERENCE_POSITION_ID TEXT NOT NULL
+
+    -- relative
+    , U_LEFT_HANDED_REFERENCE_FL BOOLEAN NOT NULL
+        DEFAULT true
+    , U_X DECIMAL(15) NOT NULL
+    , U_Y FLOAT(15) NOT NULL
+    , U_Z FLOAT(15) NOT NULL
+
+    -- global cartesian
+    , G_LEFT_HANDED_REFERENCE_FL BOOLEAN NOT NULL
+        DEFAULT true
+    , G_X FLOAT(15) NOT NULL
+    , G_Y FLOAT(15) NOT NULL
+    , G_Z FLOAT(15) NOT NULL
+
+    -- global polar
+    , G_LAT FLOAT(15) NOT NULL
+    , G_LON FLOAT(15) NOT NULL
+    , G_ALT FLOAT(15) NOT NULL
+    
+    , CREATED_TS TIMESTAMP NOT NULL
+        DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS sar.F_HL2_STAGING_PATHS;
+CREATE TABLE sar.F_HL2_STAGING_PATHS (
+    
+    DEVICE_ID CHAR(24) NOT NULL
+    , USER_ACCESS_TOKEN_ID TEXT NOT NULL
+
+    , LOCAL_WAYPOINT_1_ID INT NOT NULL
+    , LOCAL_WAYPOINT_2_ID INT NOT NULL
+    , SOURCE_FROM_SERVER_FL BOOLEAN NOT NULL
+        DEFAULT false
+    
+    -- metadata
+    , CREATED_TS TIMESTAMP NOT NULL
+        DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP SEQUENCE IF EXISTS sar.F_HL2_QUALITY_PATHS_SEQUENCE;
+CREATE SEQUENCE sar.F_HL2_QUALITY_PATHS_SEQUENCE
+    AS BIGINT
+    INCREMENT BY 1
+    START WITH 1
+;
+
+DROP TABLE IF EXISTS sar.F_HL2_QUALITY_PATHS;
+CREATE PATH sar.F_HL2_QUALITY_PATHS (
+
+    F_HL2_QUALITY_WAYPOINTS_PK INT NOT NULL
+        DEFAULT nextval('F_HL2_QUALITY_PATHS_SEQUENCE')
+
+    -- SOURCE : the first user/device which found the position
+    , SOURCE_USER_ID CHAR(24) NOT NULL
+    , SOURCE_DEVICE_ID CHAR(24) NOT NULL
+
+    -- metadata
+    , CREATED_TS TIMESTAMP NOT NULL
+        DEFAULT CURRENT_TIMESTAMP
+    
+    , PRIMARY KEY ( F_HL2_QUALITY_WAYPOINTS_PK )
+);
+
+DROP TABLE IF EXISTS sar.D_HL2_REFERENCE_POSITIONS;
+CREATE TABLE sar.D_HL2_REFERENCE_POSITIONS (
+
+    REFERENCE_POSITION_ID CHAR(24) NOT NULL
+
+    -- global cartesian (if available)
+    , G_LEFT_HANDED_REFERENCE_FL BOOLEAN NOT NULL
+        DEFAULT true
+    , G_X FLOAT(15)
+        DEFAULT null
+    , G_Y FLOAT(15)
+        DEFAULT null
+    , G_Z FLOAT(15)
+        DEFAULT null
+
+    -- global polar (if available)
+    , G_LAT FLOAT(15)
+        DEFAULT null
+    , G_LON FLOAT(15)
+        DEFAULT null
+    , G_ALT FLOAT(15)
+        DEFAULT null
+    
+    -- metadata
+    , CREATED_TS TIMESTAMP NOT NULL
+        DEFAULT CURRENT_TIMESTAMP
+    , DELETED_FL BOOLEAN NOT NULL 
+        DEFAULT false
+
+    , PRIMARY KEY ( REFERENCE_POSITION_ID )
+);
+
+DROP TABLE IF EXISTS sar.L_HL2_REFERENCE_TRANSFORMATIONS;
+/*
+    from Unity point of view:
+
+    [direct transform]
+    P_to = O_ + Q_ * ( P_from - (0,0,0) )
+
+    [inverse transform]
+    P_from = (Q_)^-1 * ( P_to - (0,0,0) ) - (Q_)^-1 * O_
+*/
+CREATE TABLE sar.L_HL2_REFERENCE_TRANSFORMATIONS (
+
+    REFERENCE_FROM_ID CHAR(24) NOT NULL
+    , REFERENCE_TO_ID CHAR(24) NOT NULL
+    , REFERENCE_LABEL_DS VARCHAR(500)
+        DEFAULT ''
+
+    -- dislocation (in cartesian coordinates)
+    , O_LEFT_HANDED_REFERENCE BOOLEAN NOT NULL
+        DEFAULT true
+    , O_X FLOAT(15) NOT NULL
+        DEFAULT 0.0
+    , O_Y FLOAT(15) NOT NULL
+        DEFAULT 0.0
+    , O_Z FLOAT(15) NOT NULL
+        DEFAULT 0.0
+    
+    -- rotation (in quaternions)
+    , Q_X FLOAT(15) NOT NULL
+        DEFAULT 1.0
+    , Q_Y FLOAT(15) NOT NULL
+        DEFAULT 0.0
+    , Q_Z FLOAT(15) NOT NULL
+        DEFAULT 0.0
+    , Q_K FLOAT(15) NOT NULL
+        DEFAULT 0.0
+    
+    -- metadata
+    , CREATED_TS TIMESTAMP NOT NULL
+        DEFAULT CURRENT_TIMESTAMP
+
+    , PRIMARY KEY ( REFERENCE_FROM_ID, REFERENCE_TO_ID )
+);
+
+DROP TABLE IF EXISTS sar.F_HL2_SERVICE_STATUS;
+CREATE TABLE sar.F_HL2_SERVICE_STATUS (
+
+    DEVICE_ID CHAR(24) NOT NULL
+    , USER_ACCESS_TOKEN_ID TEXT NOT NULL
+    
+    -- device status
+    , DEVICE_STATUS_DS VARCHAR(100) 
+        DEFAULT NULL
+    , DEVICE_STATUS_TS TIMESTAMP NOT NULL
+        DEFAULT CURRENT_TIMESTAMP
+    
+    PRIMARY KEY ( DEVICE_ID, USER_ACCESS_TOKEN_ID, DEVICE_STATUS_TS )
+);
+
+
+
+
+
+
+
+
+
+
+/* ======================================================
+
+## IoT data integration
+
+
+
+====================================================== */
